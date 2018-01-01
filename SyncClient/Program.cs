43170751730e903ffe0d77122dd3ae4f91e26755
JM.Guo <1043840980@qq.com>
2018-01-01 17:50:06 +0800

    latest

---
+++ SyncClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TCPLib;
+
+namespace SyncClient {
+    class Program {
+        static void Main(string[] args) {
+            TCPClient client = new TCPClient("127.0.0.1", 6001); 
+            //执行Start方法
+            client.Start("E:\\Temp");
+        }
+    }
+}
