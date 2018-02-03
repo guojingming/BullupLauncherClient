8339965deec727ff3ac9db9fb312cacf12ef37ca
guojingming <1043840980@qq.com>
2018-02-03 17:27:01 +0800

    latest

--- BullupVersionClient/Form1.Designer.cs
+++ BullupVersionClient/Form1.Designer.cs
@@ -278,6 +278,7 @@
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
